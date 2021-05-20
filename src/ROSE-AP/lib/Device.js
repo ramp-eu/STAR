8c3929fbb03175e98ada5177b93390efa2e817f7
Ivan Ivanov <ivan.ivanov@malonegroup.com>
2021-05-20 11:43:45 +0300

    ROSE-AP update

--- src/ROSE-AP/lib/Device.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Device = new Schema({
-    id: String,
-    unitCode: String
-});
-
-module.exports.model = mongoose.model('Device', Device);

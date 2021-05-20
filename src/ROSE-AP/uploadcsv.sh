80b644e29eca04c0c6f4afb08469647c14df64d6
Ivan Ivanov <ivan.ivanov@malonegroup.com>
2021-05-20 11:35:50 +0300

    Added latest PLC code and ROSE-AP code too

---
+++ src/ROSE-AP/uploadcsv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ip="localhost"
+id="StitchJob"
+
+csvfile="data.csv"
+echo DOING : curl create entities
+#make sure you execute this line only once, comment if you need to update existing entities
+./createcsv.sh $csvfile
+echo DOING : curl get entities
+curl --location --request GET $ip:1026/v2/entities | json_pp
+
+x=1
+while [ 1 ]
+do
+  echo "Welcome $x times"
+  x=$(( $x + 1 ))
+	sleep 1
+	echo DOING : upload csv
+  ./updatecsv.sh $csvfile
+	echo DOING : curl get entities only jobid
+	curl --location --request GET $ip:1026/v2/entities/$id/attrs/JobID | json_pp
+done
+
